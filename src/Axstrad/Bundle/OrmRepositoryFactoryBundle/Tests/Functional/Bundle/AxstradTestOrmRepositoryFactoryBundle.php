3a55142e87b04afd5f48dafba114a57840316843
Dan Kempster <dev@dankempster.co.uk>
2014-11-09 22:15:38 +0000

    Add ORM repository factory bundle.

---
+++ src/Axstrad/Bundle/OrmRepositoryFactoryBundle/Tests/Functional/Bundle/AxstradTestOrmRepositoryFactoryBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Axstrad\Bundle\OrmRepositoryFactoryBundle\Tests\Functional\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Axstrad\Bundle\OrmRepositoryFactoryBundle\Tests\Functional\Bundle\AxstradTestOrmRepositoryFactoryBundle
+ */
+class AxstradTestOrmRepositoryFactoryBundle extends Bundle
+{
+
+}
