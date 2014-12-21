a2b129ae05dcee569c82415a667f408421f2a567
Dan Kempster <dev@dankempster.co.uk>
2014-12-21 17:04:44 +0000

    Add SplObjectStorageIterator

---
+++ src/Axstrad/Component/Iterator/SplObjectStorageIterator.php
@@ -0,0 +1,149 @@
+<?php
+namespace Axstrad\Component\Iterator;
+
+use Axstrad\Component\Iterator\Exception\InvalidArgumentException;
+use Iterator;
+// use SeekableIterator;
+use SplObjectStorage;
+
+
+/**
+ * An iterator to iterate SplObjectStorage objects with the ability to decide
+ * how data is extract and present during iteratrion.
+ *
+ * @author Dan Kempster <dev@dankempster.co.uk>
+ */
+class SplObjectStorageIterator implements
+    // SeekableIterator,
+    Iterator
+{
+    const KEY_INDEX = 1;
+    const KEY_INFO = 2;
+    const KEY_OBJECT = 3;
+
+    const VALUE_OBJECT = 1;
+    const VALUE_INFO = 2;
+    const VALUE_BOTH = 3;
+
+
+    /**
+     * An array containing the entries of this collection.
+     *
+     * @var SplObjectStorage
+     */
+    private $storage;
+
+    /**
+     * @var integer
+     */
+    private $valueFlags;
+
+
+    /**
+     * Initializes a new ArrayCollection.
+     *
+     * @param SplObjectStorage $storage
+     */
+    public function __construct(SplObjectStorage $storage, $valueFlags = self::VALUE_OBJECT)
+    {
+        $this->storage = new SplObjectStorage;
+        $this->storage->addAll($storage);
+        $this->setValueFlags($valueFlags);
+    }
+
+    /**
+     * Get valueFlags
+     *
+     * @return integer
+     * @see setValueFlags
+     */
+    public function getValueFlags()
+    {
+        return $this->valueFlags;
+    }
+
+    /**
+     * Set valueFlags
+     *
+     * @param  integer $valueFlags
+     * @return self
+     * @throws InvalidArgumentException If $valueFlags is not numeric
+     * @see getValueFlags
+     */
+    public function setValueFlags($valueFlags)
+    {
+        if (!is_numeric($valueFlags)) {
+            throw InvalidArgumentException::create("integer", $valueFlags);
+        }
+
+        $this->valueFlags = (integer) $valueFlags;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function key()
+    {
+        return $this->storage->key();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function next()
+    {
+        return $this->storage->next();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function current()
+    {
+        switch ($this->getValueFlags()) {
+            case self::VALUE_INFO:
+                $return = $this->storage->getInfo();
+                break;
+
+            case self::VALUE_OBJECT:
+                $return = $this->storage->current();
+                break;
+
+            case self::VALUE_BOTH:
+                $return = array(
+                    'object' => $this->storage->current(),
+                    'info' => $this->storage->getInfo(),
+                );
+                break;
+        }
+        return $return;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rewind()
+    {
+        return $this->storage->rewind();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function valid()
+    {
+        return $this->storage->valid();
+    }
+
+    /**
+     * Returns the data associated with the current iterator entry
+     *
+     * @return mixed
+     */
+    public function getInfo()
+    {
+        return $this->storage->getInfo();
+    }
+}
